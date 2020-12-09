769e5a61f0c5ac2dbc3badf392a3f5cece9c5bda
Lourival Linard <l.linard@gmail.com>
2020-12-08 21:38:10 -0300

    Mocks

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,71 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+public class TaskControllerTest {
+	
+	@Mock
+	private TaskRepo taskRepo;
+	
+	/*
+	 * Os mocks que forem criados na classe TaskRepo, nesse contexto, 
+	 * sejam injetados na classe TaskController
+	 */
+	
+	@InjectMocks
+	private TaskController controller;
+	
+//	Vai ser executado antes de cadas teste
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemDescricao() {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemData() {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the due date", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaComDataPassada() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+		
+		Mockito.verify(taskRepo).save(todo);
+	}
+
+}
